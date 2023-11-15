108f83ca1c9a909eb315d2abd72785ada9462280
Gabriella Vieira <gabvi2015@outlook.com>
2023-11-15 10:25:12 -0300

    feat: header component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HeaderComponent } from './components/header/header.component';
+import { ReservationsComponent } from './reservations/components/reservations.component';
+import { AppMaterialModule } from './shared/app-material/app-material.module';
+import { VehiclesComponent } from './vehicles/componentes/vehicles.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    ReservationsComponent,
+    VehiclesComponent
   ],
   imports: [
+    AppMaterialModule,
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
